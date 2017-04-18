e9add02022da803bdf687c9c1890b918ec71fa88
aar9n <aaron.galper@gmail.com>
2017-04-17 21:20:35 -0700

    Added documentation, changed the entire file into a function.

--- swexplore.py
+++ swexplore.py
@@ -1,59 +1,66 @@
 from swfunc import *
 
-filmurl = "http://swapi.co/api/films"
-
-films = getAllPages(filmurl)
-print('\nFilms:')
-printItems(films, "title")
-done = False
-while not done:
-    choice = input('\n==> Enter the number of the film you would like to explore: ')
-    try:
-        if choice.lower() == 'q':
-            done = True
-            continue
-        else:
-            film = films[int(choice) - 1]
-    except:
-        print('Invalid entry: ' + choice + '. Try again!')
-        continue
-
+def main():
+    """
+    Main is the central function for prompting the user in my swapi.co explorer.
+    run this file in the terminal to use this api explorer! Main includes the
+    two outermost while loops of the explorer and the central menu, prompting
+    the user to explore either the characters, planets, vehicles, starships,
+    or attributes of any given film.
+    """
+    filmurl = "http://swapi.co/api/films"
+    films = getAllPages(filmurl)
+    print('\nFilms:')
+    printItems(films, "title")
+    done = False
     while not done:
-        print('Current film: ' + film["title"])
-        choice = input("\n==> Would you like to explore the characters (C), planets (P), vehicles (V), starships (S), or attributes (A) of %s? Select one: " % (film["title"]))
-        print('')
+        choice = input('\n==> Enter the number of the film you would like to explore: ')
+        try:
+            if choice.lower() == 'q':
+                done = True
+                continue
+            else:
+                film = films[int(choice) - 1]
+        except:
+            print('Invalid entry: ' + choice + '. Try again!')
+            continue
 
-        if choice.lower() == 'c':
-            urls = film["characters"]
-            print('Characters: ')
-            chars = getItemList(urls, "name")
-            promptForItem(chars, "character")
-
-        elif choice.lower() == 'p':
-            urls = film["planets"]
-            print('Planets: ')
-            planets = getItemList(urls, "name")
-            promptForItem(planets, "planet")
-
-        elif choice.lower() == 'v':
-            urls = film["vehicles"]
-            print('Vehicles: ')
-            vehicles = getItemList(urls, "name")
-            promptForItem(vehicles, "vehicle")
-
-        elif choice.lower() == 's':
-            urls = film["starships"]
-            print('Starships: ')
-            starships = getItemList(urls, "name")
-            promptForItem(starships, "starship")
-
-        elif choice.lower() == 'a':
-            printAttributes(film)
+        while not done:
+            print('Current film: ' + film["title"])
+            choice = input("\n==> Would you like to explore the characters (C), planets (P), vehicles (V), starships (S), or attributes (A) of %s? Select one: " % (film["title"]))
             print('')
 
-        elif choice.lower() == 'q':
-            done = True
+            if choice.lower() == 'c':
+                urls = film["characters"]
+                print('Characters: ')
+                chars = getItemList(urls, "name")
+                promptForItem(chars, "character")
 
-        else:
-            print('Invalid entry. Try again!')
-            continue
+            elif choice.lower() == 'p':
+                urls = film["planets"]
+                print('Planets: ')
+                planets = getItemList(urls, "name")
+                promptForItem(planets, "planet")
+
+            elif choice.lower() == 'v':
+                urls = film["vehicles"]
+                print('Vehicles: ')
+                vehicles = getItemList(urls, "name")
+                promptForItem(vehicles, "vehicle")
+
+            elif choice.lower() == 's':
+                urls = film["starships"]
+                print('Starships: ')
+                starships = getItemList(urls, "name")
+                promptForItem(starships, "starship")
+
+            elif choice.lower() == 'a':
+                printAttributes(film)
+                print('')
+
+            elif choice.lower() == 'q':
+                done = True
+
+            else:
+                print('Invalid entry. Try again!')
+                continue
